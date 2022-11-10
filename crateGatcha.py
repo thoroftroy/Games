3d585fc21fc57f501acb54187f7014f2832c542a
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2022-11-10 12:44:27 -0500

    Create crateGatcha.py

---
+++ crateGatcha.py
@@ -0,0 +1,18 @@
+import time
+import random
+#alrighty we are gonna make a gatha game where you unlock things
+#this gatha will be collecting crate bots
+basic = ['Crate Bot','Crate Bot MK1','Electric Crate Bot','Gas Crate Bot','Speedy Crate Bot','Broken Crate Bot','Infested Crate Bot','Tubby Crate Bot','Crate Plane','Crate Boat','Crate','Bot']
+common = ['','','','','','','','','','','',]
+uncommon = ['','','','','','','','','','','',]
+rare = ['','','','','','','','','','','',]
+superRare = ['','','','','','','','','','','',]
+ultraRare = ['','','','','','','','','','','',]
+megaRare = ['','','','','','','','','','','',]
+legendary = ['','','','','','','','','','','',]
+mythical = ['','','','','','','','','','','',]
+special = ['Worm','Worm God','Big Ben','','','','','','','','',]
+
+
+
+
