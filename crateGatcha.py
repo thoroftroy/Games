d9035d396ad03de14075c1726e034185457efd5a
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2022-11-10 12:48:04 -0500

    Update crateGatcha.py
    Basically nothing done yet

--- crateGatcha.py
+++ crateGatcha.py
@@ -2,16 +2,30 @@ import time
 import random
 #alrighty we are gonna make a gatha game where you unlock things
 #this gatha will be collecting crate bots
-basic = ['Crate Bot','Crate Bot MK1','Electric Crate Bot','Gas Crate Bot','Speedy Crate Bot','Broken Crate Bot','Infested Crate Bot','Tubby Crate Bot','Crate Plane','Crate Boat','Crate','Bot']
-common = ['','','','','','','','','','','',]
-uncommon = ['','','','','','','','','','','',]
-rare = ['','','','','','','','','','','',]
-superRare = ['','','','','','','','','','','',]
-ultraRare = ['','','','','','','','','','','',]
-megaRare = ['','','','','','','','','','','',]
-legendary = ['','','','','','','','','','','',]
-mythical = ['','','','','','','','','','','',]
+#here are the differant bots with their rarities, specials cannot be unlocked through the normal gatcha, basic will be purchased from the store just like special will but basic is super easy to come by and special is expensive. ALl other varients are throuhg the gatcha and get stronger as they go
+basic = ['Crate Bot MK1','Electric Crate Bot','Gas Crate Bot','Speedy Crate Bot','Broken Crate Bot','Infested Crate Bot','Tubby Crate Bot','Crate Plane','Crate Boat','Crate','Bot']
+common = ['Crate Bot MK2','','','','','','','','','','',]
+uncommon = ['Crate Bot MK3','','','','','','','','','','',]
+rare = ['Crate Bot MK4','','','','','','','','','','',]
+superRare = ['Crate Bot MK5','','','','','','','','','','',]
+ultraRare = ['Crate Bot MK6','','','','','','','','','','',]
+megaRare = ['Crate Bot MK7','','','','','','','','','','',]
+legendary = ['Crate Bot MK8','','','','','','','','','','',]
+mythical = ['Crate Bot MK9','','','','','','','','','','',]
 special = ['Worm','Worm God','Big Ben','','','','','','','','',]
+
+#globals
+unlocked = ['Crate Bot MK1']
+def gatcha():
+    #here we roll for a random bot
+    checkUnlocked()
+    
+    
+def checkUnlocked():
+    #here we check for what bots are unlocked/maxed out. if they are maxed out then we don't want to "unlock" them agian.
+    
+
+
 
 
 
