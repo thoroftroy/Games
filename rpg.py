eae7271e7bec1ae41ee9b0df7bc8611f53d6dfed
Rednave_b <51344077+thoroftroy@users.noreply.github.com>
2023-02-16 13:14:49 -0500

    Update rpg.py
    added in some more basic functionality

--- rpg.py
+++ rpg.py
@@ -5,7 +5,7 @@ player = {'name': '', 'health': 100, 'attack': 10, 'defense': 5}
 enemies = [{'name': 'Goblin', 'health': 50, 'attack': 5, 'defense': 2},
            {'name': 'Orc', 'health': 75, 'attack': 8, 'defense': 4},
            {'name': 'Dragon', 'health': 150, 'attack': 15, 'defense': 10}]
-
+inventory = []
 #print the stats of the player
 def print_status():
     print('\nPlayer:', player['name'], '\nHealth:', player['health'], '\nAttack:', player['attack'], '\nDefense:', player['defense'])
@@ -47,6 +47,35 @@ def fight(enemy):
         print('You died.')
     else:
         print(enemy['name'], 'defeated you.')
+#exploring which isn't entirly implemented
+def explore():
+    directions = ["north", "south", "east", "west"]
+    num_directions = len(directions)
+    random_direction = random.randint(0, num_directions-1)
+    print("You explore to the " + explore())
+    return directions[random_direction]
+#now lets make a basic inventory function
+def add_to_inventory(item):
+    inventory.append(item)
+def remove_from_inventory(item):
+    if item in inventory:
+        inventory.remove(item)
+    else:
+        print("Item not found in inventory.")
+def display_inventory():
+    print("Inventory:")
+    if not inventory:
+        print("Empty.")
+    else:
+        for item in inventory:
+            print("- " + item)
+# example usage:
+#add_to_inventory("Sword")
+#add_to_inventory("Potion")
+#display_inventory()
+#remove_from_inventory("Sword")
+#display_inventory()
+
 #the main startup funciton
 def main():
     player['name'] = input('What is your name? ')
